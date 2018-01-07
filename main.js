cdc2fa6f0cd1ad19952e5cfe4db4db2cfdd6e6aa
unknown <micbarash@gmail.com>
2018-01-07 18:47:02 +0200

    Added 'Grid Controls' panel with color choice and reset button.

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const grid = document.querySelector('#grid');
-function createDivs(amount) {
-  amount = prompt('Choose grid size');
+// This function creates the initial grid
+function drawGrid(amount) {
   for (var i = 0; i < (amount*amount); i++) {
     let div = document.createElement('div')
-    grid.appendChild(div)
-    div.onmouseover = function() {
-      div.setAttribute('style', 'background-color: black')
-    }
+    div.classList.add('pixel');
+    grid.appendChild(div);
+    div.addEventListener('mouseover', function(event) {
+      const userColor = document.getElementById('colorChoice');
+      event.target.style.background = userColor.value;
+    });
   }
   document.documentElement.style.setProperty("--columnNumber", amount);
 };
-createDivs();
+// This function clears the grid entirely
+function deleteGrid() {
+  const grid = document.querySelector('#grid')
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild)
+  }
+};
+// This function resets the grid according to user input
+function resetGrid() {
+  let userAmount = prompt('Enter grid size')*1;
+  if (userAmount > 60) {
+    alert('Beware, a grid this small will probalby look like shit')
+  }
+  deleteGrid();
+  drawGrid(userAmount);
+}
+
+drawGrid(16);
